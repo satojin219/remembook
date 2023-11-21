0a8e28df95cad3c37ce05541bb9b7171f53d8dab
satojin219 <renzuoteng142@gmail.com>
2023-11-21 12:22:20 +0900

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    externalDir:
+      true |
+      {
+        optimizeFonts: true,
+        enabled: true,
+        silent: true,
+      },
+  },
+}
 
 module.exports = nextConfig
