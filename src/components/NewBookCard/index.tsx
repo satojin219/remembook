c2e2582bff8bd99ad395d96f0344ce5b184c9021
satojin219 <renzuoteng142@gmail.com>
2023-11-21 23:16:59 +0900

    fix: newBooks router

--- src/components/NewBookCard/index.tsx
+++ src/components/NewBookCard/index.tsx
@@ -5,7 +5,7 @@ import { container, title } from "./newBookCard.css";
 
 export const NewBookCard: React.FC = () => {
   return (
-    <Link className={container} href={`books/newBook`}>
+    <Link className={container} href={`/newBook`}>
       <Image src={"/Book.svg"} alt="表紙の画像" width={140} height={140}></Image>
       <p className={title}>本を追加する</p>
     </Link>
